7a613e9f72e01abc55b60c1d0d75e32ca94c2871
Restituto <maganrodeo@gmail.com>
2024-05-08 14:44:19 +0800

    initial commit

---
+++ app.py
@@ -0,0 +1,277 @@
+import json
+import streamlit as st
+from streamlit_lottie import st_lottie
+from pathlib import Path
+from PIL import Image
+from styles import css_styles as css
+
+st.set_page_config(
+        page_title="Rodeo's Portfolio",
+        page_icon=":smiley:",
+        layout="wide",
+        initial_sidebar_state="expanded",
+        )
+
+############################### PATH 
+current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
+css_file = current_dir / "styles" / "main.css"
+resume_file = current_dir / "assets" / "RestitutoPDF.pdf"
+earthquake_data = current_dir / "images"/ "good_tableau project.PNG"
+covid_data = current_dir / "images"/ "covid_data.PNG"
+profile = current_dir  / "images"/ "profile.jpg"
+freecodecamp_cert = current_dir / "images" / "certificates" / "cert" / "data_analysis_certification_freecodecamp.PNG"
+Coursera_cert_financial_analysis = current_dir / 'images' / 'certificates'/ 'other_cert' / 'Coursera_PSFA.PNG'
+banner = current_dir / "images" / "banner.png"
+
+with open(css_file) as f:
+    css_content = f.read()
+st.markdown(f"<style>{css_content}</style>", unsafe_allow_html=True)
+
+with open(resume_file, "rb") as pdf_file:
+    PDFbyte = pdf_file.read()
+image = Image.open(profile)
+earthquake_image = Image.open(earthquake_data)
+covid_image = Image.open(covid_data)
+fcc = Image.open(freecodecamp_cert)
+C_PSFAs = Image.open(Coursera_cert_financial_analysis)
+ban = Image.open(banner)
+################################ Banner
+
+
+
+################################ Links
+earthquake_link = "https://public.tableau.com/views/Philippine_Earthquake_Data2020-2024/Dashboard1?:language=en-US&:sid=&:display_count=n&:origin=viz_share_link"
+covid_link = "https://public.tableau.com/views/CovidDashboard_17084094170250/Dashboard1?:language=en-US&:sid=&:display_count=n&:origin=viz_share_link"
+
+################################ lottie function
+def get(lottie:str):
+    with open(lottie, "r") as p:
+        return json.load(p)
+
+lottie = get("./lottie_animation.json")
+################################ Functions
+
+def center_text(text):
+    with open(css_file) as f:
+        css_content = f.read()
+    return f'<style>{css_content}</style><div class="centered-text">{text}</div>'
+
+def txt(a,b,c,link):
+    col1 , col2 = st.columns([1,1])
+    with col1:
+        st.write("#")
+        st.image(a)
+    with col2:
+        st.header(b)
+        st.write("###")
+        st.write("#")
+        st.write('\n')
+        st.caption(c)
+        st.markdown(f"[Go to Project...]({link})")
+
+def txt1(a,b):
+    txtcol1, txtcol2 = st.columns([1,1])
+    with txtcol1:
+        st.write("#")
+        st.write(a)
+    with txtcol2:
+        st_lottie(b, height=400, key="user")
+
+def textcolcon(title,details,link):
+    with st.container():
+        txtcol1 = st.columns(1)
+        with txtcol1[0]:
+            st.write("#")
+            st.write(title)
+            st.write('---')
+            st.write(details)
+            st.write(f'##### [Go to Certificate...]({link})')
+################################ Assets
+
+st.markdown(css.header, unsafe_allow_html=True)
+st.write("###")
+with st.container():
+    # Layout
+    st.markdown(css.new_banner_html, unsafe_allow_html=True)
+    # CSS styling
+    #st.markdown(css.new_banner_css, unsafe_allow_html=True)
+st.write("###")
+
+
+# About Me
+st.markdown(css.about_me,unsafe_allow_html=True)
+st.write("###")
+
+with st.container():
+    col1 , col2 = st.columns([1,2])
+    with col1:
+        st.image(image)
+    with col2:
+        st.markdown(css.Name,unsafe_allow_html=True)
+        st.write("---")
+        st.write("###")
+        st.caption("As a budding data analyst, I prioritize trust and integrity, ensuring your data's confidentiality and security. Despite my introverted nature, I excel in visualization, transforming data into compelling insights with clarity and precision.")
+        st.caption("With a commitment to continuous learning, I'm dedicated to delivering impactful results that drive informed decision-making.")
+        st.markdown(css.details_aboutme,unsafe_allow_html=True)
+        st.download_button(
+            label=" 📄 Download Resume",
+            data=PDFbyte,
+            file_name=resume_file.name,
+            mime="application/octet-stream")
+
+st.write("###")
+st.write("#")
+st.write('\n')
+st.markdown(css.projects,unsafe_allow_html=True)
+st.write("###")
+st.write("#")
+st.write('\n')
+
+##################### Projects
+
+Tableau,PowerBi , Excel , Python = st.tabs(["Tableau" , "Power Bi", "Excel" , "Python"])
+with st.container():
+    with Tableau:
+        txt(earthquake_image,
+            " Analyzing Seismic Activity: \n A Study of Earthquakes in the Philippines (2020-2024)",
+            """Exploring the Dynamics of Earthquake Occurrences and Patterns in the Philippines
+            over a Five-Year Period, Unveiling Insights into Seismic Activity and Its Implications
+                for Disaster Preparedness and Mitigation Efforts.""",earthquake_link)
+
+        st.write("#")
+        st.write("#")
+
+        txt(covid_image,
+            " Global COVID-19 Mortality Trends: \n Analysis of Deaths and Fatality Rates Across Continents",
+            """Exploring the Dynamics of COVID-19 Mortality Patterns and Fatality Rates on a Global Scale, 
+            Unveiling Insights into Regional Disparities and Implications for Public Health Strategies.""",covid_link)
+
+ # not yet finnish missig excel and python
+
+##################### Skills
+
+st.write("###")
+st.write("#")
+st.write('\n')
+st.markdown(css.skills,unsafe_allow_html=True)
+st.write("###")
+st.write("#")
+st.write('\n')
+
+
+skills1= """
+- ##### Programming: Python (basic), SQL (basic querying)
+- ##### Data Manipulation:  Basic cleaning techniques, Pandas (Python)
+- ##### Visualization:  Matplotlib, Tableau/Power BI, Streamlit (basic)
+- ##### Statistical Analysis:  Basic concepts (mean, median, standard deviation), hypothesis testing
+- ##### Database Management: SQL querying, data extraction/filtering
+- ##### Data Analysis Tools: Jupyter Notebook
+- ##### Soft Skills: Problem-solving, attention to detail, eagerness to learn
+"""
+st.markdown(css.hr, unsafe_allow_html=True)
+txt1(skills1,lottie)
+
+######################## Certificates
+st.write("###")
+st.write("#")
+st.write('\n')
+st.markdown(css.certificates,unsafe_allow_html=True)
+ # textcolcon(image,title,details,link)
+textcolcon('- ## Data Analysis with Python', 
+'##### Skill gain: Data Cleaning, Data Exploration, Visualizing Data, Python basics',
+'https://www.freecodecamp.org/certification/Restituto_Rodeo_jr/data-analysis-with-python-v7')
+
+st.write("###")
+st.write("#")
+st.write('\n')
+######################## Other Certificates
+st.write("###")
+st.write("#")
+st.write('\n')
+st.markdown(css.other_certificates,unsafe_allow_html=True)
+# textcolcon(image,title,details,link)
+
+textcolcon('- ## Python and Statistics for Financial Analysis',
+            '##### Skills gain: Financial Data Analysis,Financial Analysis, Python Programming, Statistical Analysis, Data Visualization',
+            'https://coursera.org/share/ad6dc78ca997f01f71365acbde55b814')
+
+textcolcon('- ## Introduction to Statistics', 
+           '##### Skills gain: Exploratory Data Analysis, Statistical Thinking Concepts, ',
+           'https://coursera.org/share/69b40a6c47244c5c0e8187845b76d8b6')
+st.write("###")
+st.write("#")
+st.write('\n')
+######################## Education
+st.write("###")
+st.write("#")
+st.write('\n')
+st.markdown(css.education,unsafe_allow_html=True)
+with st.container():
+    c = st.columns(1)
+    with c[0]:
+        st.markdown('## Bachelor of Science in Computer Engineering')
+        st.markdown('##### (Undergraduate)')
+        st.markdown('##### AMA University Caloocan Campus')
+        st.markdown('##### MX5H+328, Caimito Road, Samson Rd, Caloocan, Metro Manila')
+
+
+st.write("###")
+st.write("#")
+st.write('\n')
+######################## Contact Information
+st.write("###")
+st.write("#")
+st.write('\n')
+st.markdown(css.contact,unsafe_allow_html=True)
+st.write("###")
+st.write("#")
+st.write('\n')
+
+
+with st.container():
+    address, phone, email = st.columns(3)
+    with address:
+        st.write(" #### Address" )
+        st.markdown(css.hr, unsafe_allow_html=True)
+        st.caption(" #### Manila ,Philippines")
+    with phone:
+        st.write(" #### Phone")
+        st.markdown(css.hr, unsafe_allow_html=True)
+        st.caption(" #### +639602498779")
+        
+    with email:
+        st.write(" #### Email")
+        st.markdown(css.hr, unsafe_allow_html=True)
+        st.caption(" #### restitutorodeo@gmail.com")
+
+with st.container():
+    st.subheader("Get in Touch With Me!")
+    left_col1 , right_col2 = st.columns(2)
+    with left_col1:
+        st.markdown(css.contact_form, unsafe_allow_html=True)
+    with right_col2:
+        st.markdown(css.Nameko,unsafe_allow_html=True)
+        st.write("---")
+        st.write("###")
+        st.markdown(css.bible_verse,unsafe_allow_html=True)
+
+
+st.write("#")
+st.write("#")
+st.write('\n')
+st.markdown(css.hr, unsafe_allow_html=True)
+
+st.markdown(css.font_awesome_link,unsafe_allow_html=True)
+st.markdown(css.social_links, unsafe_allow_html=True)
+
+
+st.write("#")
+st.write("#")
+st.write('\n')
+
+
+
+
+
+
+
---
+++ styles/css_styles.py
@@ -0,0 +1,361 @@
+
+
+header = """
+        <div class="nav-bar">
+                <div class="flex-Container">
+                        <div><span class="fname">Restituto</span>Rodeo Jr.</div>
+                </div>
+        </div>
+        
+        <style>
+        :root {
+                --max-width: 1900px; /* Default maximum width */
+        }
+        .st-emotion-cache-z5fcl4{padding: 0;}
+        .st-emotion-cache-v09qz{gap:0;}
+
+        .nav-bar {
+                position: fixed;
+                top:0;
+                left:0;
+                right:0;
+                background-color: rgba(0, 0, 0, 0.5);
+                max-width: var(--max-width);
+                margin: 0 auto;
+                z-index: 3;
+        }
+        .nav-bar > .flex-Container{      
+                display: flex;
+                align-items: center;}
+        .flex-Container{
+                width: 70%;
+                padding: 25px 0;
+                margin:0 auto;
+        }
+        .flex-Container > div > .fname {color:#ff5d56;}
+        .flex-Container > div {
+                font-family: 'Berkshire Swash', cursive;
+                color: White;
+                margin: 0px;
+                padding: 2.5px;
+                font-size: 30px;}
+        h2{Color:black}
+        </style>
+        """
+
+# banner = """
+#         <div class="banner">
+#         </div>
+#         <style>
+#         .banner{
+#                 position: relative;
+#                 top: 0px;
+#                 left: 0px;
+#                 width: 100%;
+#                 background:url('./images/banner.png') no-repeat top;
+#                 background-size:cover;
+#                 background-position: center;
+#                 padding: 550px 0 120px;
+#                 }
+#         </style>
+# """
+
+animated_word = """
+        <div>
+                <span class="want-to-be">
+                        Data Analyst
+                </span>
+        </div>
+        <style>
+        .want-to-be{
+                font-size:5rem;
+                padding: 1rem:
+                margin: 0;
+                color: #ff5d56;
+                }
+        </style>
+
+"""
+
+about_me = """
+        <div class="center1">
+                <h1 class="about">About Me</h1>
+                <span>Professional Profile - There Is All About Me</span>
+        </div>
+        
+        <style>
+        
+        .about {
+                font-family: 'Berkshire Swash', cursive;
+                font-style: normal;
+                font-weigth: 20px;
+                font-size: 35px;
+                }
+        </style>
+"""
+
+details_aboutme = """
+        <div class="flex-container">
+                <div class="flex-columns">
+                        <ul>
+                                <li><b>Date of birth:</b> 10 March 2000</li>
+                                <li><b>Nationality:</b> Filipino</li>
+                                <li><b>Freelance:</b> N/A</li>
+                                <li><b>Phone:</b> +639602498770</li>
+                        </ul>
+                </div>
+                <div class="flex-columns">
+                        <ul>
+                                <li><b>Address:</b> Manila, Philippines</li>
+                                <li><b>Email:</b> restitutorodeo@gmail.com</li>
+                                <li></i><b>Spoken Langages:</b> Tagalog, English</li>
+                                <li><b>Telegram:</b> <i><a href="https://t.me/Restituto_Rodeo" class="link">@Restituto_Rodeo</a></i></li>
+                        </ul>
+                </div>
+
+        </div>
+        <style>
+        .link{
+                text-decoration: none;
+        }
+        .link:hover{
+                text-decoration: none;
+        }
+        
+        .flex-container{
+                display: flex;
+                width: 100%;
+                margin: 15px 0;
+        }
+        .flex-columns{
+                width: 100%;
+                margin: 0 20px;
+        }
+        </style>
+"""
+
+#--------------------------------------------------------------------------------
+new_banner_html = """
+        <div class="container">
+                <div class="column2">
+                    <div class="column1">
+                        <div class="col-1">
+                                <h3 class="h3">Hello There! I\'m </b>Aspiring</b></h3>
+                                <span class="want-to-be">Data Analyst</span>
+                                <span>I organize data for analysis and I\'m eager to learn more. I want to help teams make better decisions using various tools like Excel for data manipulation, SQL for database querying, and visualization tools like Tableau, Power BI, and Python for analysis.</span>
+                        </div>
+                    </div>
+                </div>
+        </div>
+                """
+
+new_banner_css = """
+        <style>
+                .container {
+                        top: 0px;
+                        left: 0px;
+                        height: 70%; /* Take full height of parent container */
+                        background: url("../images/banner.png") no-repeat top;
+                        background-size:cover;
+                        background-position: center;
+                        z-index: 0; /* Ensure column2 appears below column1 */
+                }
+                .column1 {
+                        display: flex;
+                        width: 70%;
+                        height: 70vh; /* Take full height of parent container */
+                        margin: 0 auto;
+                        align-items: center;
+                        /* background-color: red;*/
+                        z-index: 1; /* Ensure column1 appears above column2 */
+                }
+                .column2 {
+                        position: relative;
+                        width: 100%;
+                }
+                .col-1 {
+                        display: grid;
+                        margin: 0 50% 0 0;
+                }
+                .h3 {
+                        margin:0;
+                        padding: 0;
+                }
+                .want-to-be{
+                        font-size:5rem;
+                        padding: 0;
+                        margin: 0;
+                        color: #ff5d56;
+                }
+        </style>
+    """
+
+Name = """
+        <div>
+                <span class="h3">I'm Restituto Rodeo Jr.</span>
+        </div>    
+        <style>.h3{font-size: 30px;}</style>            
+"""
+Nameko = """
+        <div class="nmae-con">
+                <span class="nmae">Restituto Rodeo Jr.</span>
+        </div>    
+        <style>.nmae{font-size: 30px;} 
+                .nmae-con{text-align: center;}</style>            
+"""
+
+projects = """
+        <div class="center1">
+                <h1 class="about">Personal Projects</h1>
+                <span></span>
+        </div>
+        
+        <style>
+        .about {
+                font-family: 'Berkshire Swash', cursive;
+                font-style: normal;
+                font-size: 35px;
+                }
+        </style>
+"""
+skills = """
+        <div class="center1">
+                <h1 class="about">Skills</h1>
+                <span></span>
+        </div>
+        
+        <style>
+        .about {
+                font-family: 'Berkshire Swash', cursive;
+                font-style: normal;
+                font-size: 35px;
+                }
+        </style>
+"""
+
+certificates = """
+        <div class="center1">
+                <h1 class="about">Certificates</h1>
+                <span></span>
+        </div>
+        
+        <style>
+        .about {
+                font-family: 'Berkshire Swash', cursive;
+                font-style: normal;
+                font-size: 35px;
+                }
+        </style>
+"""
+other_certificates = """
+        <div class="center1">
+                <h1 class="about">Other Certificates</h1>
+                <span></span>
+        </div>
+        
+        <style>
+        .about {
+                font-family: 'Berkshire Swash', cursive;
+                font-style: normal;
+                font-size: 35px;
+                }
+        </style>
+"""
+
+education = """
+        <div class="center1">
+                <h1 class="about">Education</h1>
+                <span></span>
+        </div>
+        
+        <style>
+        .about {
+                font-family: 'Berkshire Swash', cursive;
+                font-style: normal;
+                font-size: 35px;
+                }
+        </style>
+"""
+
+contact = """
+        <div class="center1">
+                <h1 class="about">Contact</h1>
+                <span></span>
+        </div>
+        
+        <style>
+        .about {
+                font-family: 'Berkshire Swash', cursive;
+                font-style: normal;
+                font-size: 35px;
+                }
+        </style>
+"""
+
+
+hr = """
+        <hr class="hr"></hr>
+        <style>
+        .hr {
+                max-width: var(--max-width);
+                width: 70%;
+                margin: 2em auto;
+                border-bottom: 2px solid #ff5d56;
+}
+        </style>
+"""
+
+contact_form = """
+    <form action="https://formsubmit.co/restitutorodeo@gmail.com" method="POST">
+    <input type="hidden" name="_captcha" value="false">
+     <input type="text" name="name" placeholder="Your Name" required>
+     <input type="email" name="email" placeholder="Your Email" required>
+     <textarea name="message" placeholder="Your Message here" required></textarea>
+     <button type="submit">Send</button>
+</form>
+
+"""
+
+bible_verse = """
+        <div class="center_text">
+                <h5><b>Philippians 4:6</b></h5>
+                <p><em>Don't worry about anything: instead pray about everything. Tell God what you need, and thank Him for all He has done.</em></p>
+        </div>
+        <style>
+                .center_text{
+                        text-align: center;
+                        margin: 0 10%;
+                }
+        </style>
+"""
+
+font_awesome_link ="""
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+"""
+
+social_links = """
+        <div class="link-con">
+                <div class="link-child">
+                        <a href="https://t.me/Restituto_Rodeo"><i class="fa fa-telegram icon"></i></a>
+                        <a href="https://github.com/magan10"><i class="fa fa-github icon"></i></a>
+                        <a href="https://www.linkedin.com/in/restituto-rodeo-jr/"><i class="fa fa-linkedin-square icon"></i></a>
+                </div>
+        </div>
+
+        <style>
+                .link-con{
+                        position: relative;
+                        width: 70%;
+                        margin: 0 auto;
+                }
+                .link-child {
+                        text-align: center;
+                }
+                .icon{
+                        font-size:36px;
+                        color: #ff5d56;
+                        padding: 0 20px;
+                }
+        </style>
+"""
+
